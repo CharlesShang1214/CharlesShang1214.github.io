7f678e85de2b8eb369d5e5d5adf9152bbb9a7c65
CharlesShang1214 <144711618+CharlesShang1214@users.noreply.github.com>
2024-04-24 16:42:39 +0000

    ""

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
-console.log(Task1)
+console.log("Task1")
 for (let i = 0;i<=10;i++){
 
 
     console.log(i*i);
 }
-
+console.log("Task2")
 for(let i=6;i<0;i--){
     console.log(i)
 }
 console.log("Blastoff!")
-console.log(Task3)
+console.log("Task3")
 for (let i=0;i<=25;i++){
 
 
