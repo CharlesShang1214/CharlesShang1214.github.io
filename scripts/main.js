262b037dad997804554a32f7c724d93a19b89a7b
CharlesShang1214 <144711618+CharlesShang1214@users.noreply.github.com>
2024-04-24 16:20:04 +0000

    js

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+Task1
+for (var i = 0;i<=10;i++){
+    i*i
+}
+
+
+Task2
+
+Task3
+for (var i=0;i<=25;i++){
+    i*2
+}
+
+
+Task4
+    
+Task5 
+for (var i=0;i<=10;i++){
+    for (var j=i;j>0;i--)
+}
