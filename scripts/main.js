d6a6cd53e39129fbdfa45d097bbc5ea0256bdc97
CharlesShang1214 <144711618+CharlesShang1214@users.noreply.github.com>
2024-04-24 16:39:25 +0000

    fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,27 @@
-
+console.log(Task1)
 for (let i = 0;i<=10;i++){
-    i*i
-    console.log(Task1)
-    console.log(i);
+
+
+    console.log(i*i);
 }
 
 for(let i=6;i<0;i--){
     console.log(i)
 }
 console.log("Blastoff!")
-
+console.log(Task3)
 for (let i=0;i<=25;i++){
-    i*2
 
-    console.log(Task3)
-    console.log(i);
+
+
+    console.log(i*2);
 }
 
+for (let i=0;i<=100;i++){
+    for(let j=i;j<=100;)
+}
 
+for (let i=0;i<=10;i++){
+    
+}
 
